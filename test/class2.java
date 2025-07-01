75a613585001c6371ea97f47cede89a0f94fb985
Promod <contactpromods@gmail.com>
2025-07-02 00:03:20 +0530

    3rd commit

--- test/class2.java
+++ test/class2.java
@@ -1,3 +1,9 @@
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
+public class class2 {
+     // This is a placeholder class to demonstrate the structure.
+    // You can add methods and properties as needed.
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
+
